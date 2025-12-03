953e5fc00a92a2bd92ae68e5201e3bd3d79f8418
Gaurav Singh Yadav <137716439+EGAVSIV@users.noreply.github.com>
2025-12-03 18:34:38 +0530

    Update IDR.py

--- IDR.py
+++ IDR.py
@@ -185,8 +185,7 @@ body {background-color: #111;}
 </style>
 """, unsafe_allow_html=True)
 
-st.title(" INTRADAY REVERSAL TIME
-CREATED BY RAO SAAB ")
+st.title("INTRADAY REVERSAL TIME CREATED BY RAO SAAB ")
 
 # Date selection
 col1, col2, col3 = st.columns(3)
